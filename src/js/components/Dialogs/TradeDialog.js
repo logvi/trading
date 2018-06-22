e7d066d0275db0910cb8d4675b0ec02acbd5b745
Vitaly <mirenrich@gmail.com>
2018-06-22 14:41:31 +0300

    trade dialog

--- src/js/components/Dialogs/TradeDialog.js
+++ src/js/components/Dialogs/TradeDialog.js
@@ -30,7 +30,8 @@ class TradeDialog extends Component {
   };
 
   render() {
-    const {type, amount} = this.props.trade;
+    const {type, amount, timeOpen, timeClose, priceOpen, priceClose, stopLoss, volume, stopLossVolume, profit} = this.props.trade;
+    const {symbol, priceStep, priceStepCost, go} = this.props.trade.symbol;
 
     return (
       <Dialog
@@ -48,21 +49,36 @@ class TradeDialog extends Component {
               <div>
                 <TextField
                   label="Symbol"
+                  value={symbol}
+                  inputProps={{name: 'symbol'}}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
                   label="Price Step"
+                  type="number"
+                  value={priceStep}
+                  inputProps={{name: 'priceStep'}}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
                   label="Cost of price step"
+                  type="number"
+                  value={priceStepCost}
+                  inputProps={{name: 'priceStepCost'}}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
                   label="GO"
+                  type="number"
+                  value={go}
+                  inputProps={{name: 'go'}}
+                  onChange={this.onFieldChange}
                 />
               </div>
             </div>
@@ -92,26 +108,46 @@ class TradeDialog extends Component {
               <div>
                 <TextField
                   label="Open Price"
+                  inputProps={{name: 'priceOpen'}}
+                  type="number"
+                  value={priceOpen}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
+                  type="datetime-local"
                   label="Open Time"
+                  inputProps={{name: 'timeOpen'}}
+                  value={timeOpen}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
                   label="SL Price"
+                  inputProps={{name: 'stopLoss'}}
+                  type="number"
+                  value={stopLoss}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
                   label="Close Price"
+                  inputProps={{name: 'priceClose'}}
+                  type="number"
+                  value={priceClose}
+                  onChange={this.onFieldChange}
                 />
               </div>
               <div>
                 <TextField
+                  type="datetime-local"
+                  value={timeClose}
+                  inputProps={{name: 'timeClose'}}
                   label="Close Time"
+                  onChange={this.onFieldChange}
                 />
               </div>
             </div>
@@ -121,16 +157,25 @@ class TradeDialog extends Component {
             <div>
               <TextField
                 label="Volume"
+                value={volume}
+                type="number"
+                disabled
               />
             </div>
             <div>
               <TextField
                 label="SL Volume"
+                value={stopLossVolume}
+                type="number"
+                disabled
               />
             </div>
             <div>
               <TextField
                 label="Profit"
+                value={profit}
+                type="number"
+                disabled
               />
             </div>
           </div>
--- src/js/store/TradeStore.js
+++ src/js/store/TradeStore.js
@@ -1,17 +1,62 @@
-import {observable, action} from 'mobx';
+import {observable, action, autorun} from 'mobx';
 
 class TradeStore {
   @observable ticket = null;
   @observable amount = 0;
   @observable type = 'BUY';
+  @observable priceOpen = 0;
+  @observable timeOpen = new Date().toJSON().split('.')[0];
+  @observable priceClose = 0;
+  @observable timeClose = new Date().toJSON().split('.')[0];
+  @observable stopLoss = 0;
+  @observable volume = 0;
+  @observable stopLossVolume = 0;
+  @observable profit = 0;
+  @observable symbol = {
+    symbol: '',
+    priceStep: 1,
+    priceStepCost: 1,
+    go: ''
+  };
+
+  constructor() {
+    autorun(() => this.calculateVolume());
+    autorun(() => this.calculateStopLossVolume());
+    autorun(() => this.calculateProfit());
+  }
+
+  calculateVolume = () => {
+    const price = this.symbol.go || this.priceOpen;
+    const volume = this.amount * price;
+    this.setValue('volume', volume);
+  };
+
+  calculateStopLossVolume = () => {
+    if (!this.stopLoss) return;
+    const res = (Math.abs(this.priceOpen - this.stopLoss))/this.symbol.priceStep*this.symbol.priceStepCost*this.amount;
+    this.setValue('stopLossVolume', res);
+  };
+
+  calculateProfit = () => {
+    if (!this.priceClose) return;
+    const koef = this.type === 'BUY' ? -1 : 1;
+    const res = (this.priceOpen - this.priceClose)/this.symbol.priceStep*this.symbol.priceStepCost*this.amount*koef;
+    this.setValue('profit', res);
+  };
 
   @action setTicket = value => {
     this.ticket = value;
   };
 
   @action setValue = (field, value) => {
+    if (this.symbol.hasOwnProperty(field)) return this.setSymbol(field, value);
     if (!this.hasOwnProperty(field)) return;
     this[field] = value;
+  };
+
+  @action setSymbol = (field, value) => {
+    if (!this.symbol.hasOwnProperty(field)) return;
+    this.symbol[field] = value;
   };
 }
 
