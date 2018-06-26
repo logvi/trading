c7053bfad32a6ef3bcebc362c1c19faf1a184baf
Vitaly <mirenrich@gmail.com>
2018-06-26 16:46:08 +0300

    change db

--- server/db.js
+++ server/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://vitalik:%40Mi1919900990ReN'
-  + '@cluster0-shard-00-00-4p1ds.mongodb.net:27017,cluster0-shard-00-01-4p1ds.mongodb.net:27017,cluster0-shard-00-02-4p1ds.mongodb.net:27017/'
-  + 'trading'
-  + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+const dbConfig = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+};
+
+if (!dbConfig.username || dbConfig.password) throw new Error('no db config');
+
+const uri = `mongodb://${dbConfig.username}:${dbConfig.password}
+@cluster0-shard-00-00-4p1ds.mongodb.net:27017,cluster0-shard-00-01-4p1ds.mongodb.net:27017,cluster0-shard-00-02-4p1ds.mongodb.net:27017/trading?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
 
 mongoose.connect(uri).catch(err => {});
 
