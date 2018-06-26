8b5e37724b6ec1c863244be9be173e65d2aa9a07
Vitaly <mirenrich@gmail.com>
2018-06-26 14:04:38 +0300

    server: set/get methods

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # trading
+
+1. (+) Server: getTrades by timeOpen
+2. (+) Server: getTrade by id
+3. (+) Server: setTrade by id
+4. (+) Server: getCount of open/closed trades
+5. Server: getTotalProfit (with period filter)
+6. Server: getOpenVolume
+7. Server: getOpenStopLossVolume
+8. Organize communication between server and client (websocket)
--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ const Symbol = require('./Symbol');
 function setTrade(data) {
   return new Promise((resolve, reject) => {
     const symbolData = data.symbol;
+    const options = {upsert: true, new: true, setDefaultsOnInsert: true};
     Symbol.findOneAndUpdate({symbol: symbolData.symbol}, {
       symbol: symbolData.symbol,
       priceStep: symbolData.priceStep,
       priceStepCost: symbolData.priceStepCost,
       go: symbolData.go
-    }, {upsert: true, new: true, setDefaultsOnInsert: true}).then(symbol => {
-      const trade = new Trade({
+    }, options).then(symbol => {
+      Trade.findOneAndUpdate({_id: data._id},{
         amount: data.amount,
         type: data.type,
         priceOpen: data.priceOpen,
@@ -23,24 +24,42 @@ function setTrade(data) {
         stopLossVolume: data.stopLossVolume,
         profit: data.profit,
         symbol: symbol._id
-      });
-
-      return trade.save().then(trade => resolve(trade)).catch(err => reject(err));
+      }, options).then(trade => resolve(trade)).catch(err => reject(err));
     });
   });
 }
 
-setTrade({
-  symbol: {
-    symbol: 'SIM8',
-    priceStep: 0.2,
-    priceStepCost: 3,
-    go: 4000
-  },
-  amount: 10,
-  type: 'SELL',
-  priceOpen: 206.58,
-  timeOpen: new Date()
-}).then(result => {
-  console.log('success', result)
-}).catch(err => console.log('error', err));
+function getTrades(params = {}) {
+  const {filter = {}} = params;
+  if (filter._id) return Trade.findById(filter._id).populate('symbol');
+  return Trade.find(filter).populate('symbol').sort({timeOpen: -1});
+}
+
+function getTradesCount() {
+  return Trade.count();
+}
+
+function getOpenTradesCount() {
+  return Trade.count(({timeClose: null}));
+}
+
+function getClosedTradesCount() {
+  return Trade.count(({timeClose: {$ne: null}}));
+}
+
+// setTrade({
+//   symbol: {
+//     symbol: 'SIM8',
+//     priceStep: 0.2,
+//     priceStepCost: 3,
+//     go: 4000
+//   },
+//   amount: 10,
+//   type: 'SELL',
+//   priceOpen: 206.58,
+//   timeOpen: new Date()
+// }).then(result => {
+//   console.log('success', result)
+// }).catch(err => console.log('error', err));
+
+// getTrades().then(trades => console.log(trades));
